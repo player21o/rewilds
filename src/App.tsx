384ebafd1e3835818176678d54fbf83ccc7c978a
Your Name <196048914+player21o@users.noreply.github.com>
2025-11-13 15:34:18 +0100

    feat: introduced config for easy lobby switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import Lobby from "./Lobby";
 import Game from "./Game";
-import { useState } from "preact/hooks";
+import { useEffect, useState } from "preact/hooks";
 
 const App = () => {
   const [gameUrl, setGameUrl] = useState<null | string>(null);
+  const [lobbyUrl, setLobbyUrl] = useState<null | string>(null);
 
-  return gameUrl == null ? (
-    <Lobby url="ws://localhost:8888" onPlay={(url) => setGameUrl(url)}></Lobby>
-  ) : (
-    <Game url={gameUrl}></Game>
-  );
+  useEffect(() => {
+    fetch("/env.json").then((r) => {
+      r.json().then((j) => {
+        setLobbyUrl(j.lobby_url);
+      });
+    });
+  });
+
+  return lobbyUrl != null ? (
+    gameUrl == null ? (
+      <Lobby url={lobbyUrl} onPlay={(url) => setGameUrl(url)}></Lobby>
+    ) : (
+      <Game url={gameUrl}></Game>
+    )
+  ) : null;
 };
 
 export default App;
